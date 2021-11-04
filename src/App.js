c44cbc17820415c1ac6549b73dc29b598aa89a8b
Tony Ingle <tonyingle@Tonys-MacBook-Air.local>
2021-11-04 09:26:20 -0600

    adjust viewport height/width before adding color

--- src/App.js
+++ src/App.js
@@ -146,7 +146,7 @@ const App = () => {
 	const addToFavorites = (weatherSearch) => {
 		const newFavoritesList = [...favorites, weatherSearch];
 		setFavorites(newFavoritesList);
-		console.log(newFavoritesList);
+		console.log(newFavoritesList); // Delete in the future.
 	};
 
 	return (
